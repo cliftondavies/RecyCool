4252b16827f1324c9333f3e452d3c8ca07c50556
cliftondavies <clifton.davies@outlook.com>
2021-03-06 21:37:02 +0000

    Implement postlist component

---
+++ src/components/PostList/PostList.js
@@ -0,0 +1,42 @@
+import { useEffect } from 'react';
+import { useSelector, connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Post from '../Post/Post';
+import filterPostsByCategory from '../../redux/filters';
+import { getLondonPosts, getUserCoords } from '../../api/api';
+
+const PostList = ({
+  dispatch, postType, limited,
+}) => {
+  const { defaultLoading, geoLoading, error } = useSelector(state => state.posts);
+  let posts = useSelector(state => filterPostsByCategory(state, postType));
+  posts = (limited) ? posts.splice(0, 6) : posts;
+
+  useEffect(() => {
+    if (defaultLoading === 'idle') {
+      dispatch(getLondonPosts);
+    } else if (geoLoading === 'idle') {
+      dispatch(getUserCoords);
+    }
+  }, [defaultLoading, geoLoading, dispatch]);
+
+  return (
+    <div>
+      {(posts.length > 0) ? posts.map(post => (
+        <Post post={post} postType={postType} key={post.post_id} />
+      )) : <span>{error}</span>}
+    </div>
+  );
+};
+
+PostList.defaultProps = {
+  limited: false,
+};
+
+PostList.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  postType: PropTypes.string.isRequired,
+  limited: PropTypes.bool,
+};
+
+export default connect(null, null)(PostList);
