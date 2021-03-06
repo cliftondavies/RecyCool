39dc5d8d4e84c9f524903cd5870bae792d4983f5
cliftondavies <clifton.davies@outlook.com>
2021-03-06 21:26:48 +0000

    Update filters

--- src/redux/filters.js
+++ src/redux/filters.js
@@ -1,5 +1,4 @@
 export default (state, filter) => {
   const { posts } = state.posts;
-  if (posts.length > 0) { return posts.filter(post => post.type === filter); }
-  return posts;
+  return (posts.length > 0) ? posts.filter(post => post.type === filter) : posts;
 };
