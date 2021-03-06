0eb87eed3d0077581d22e07f984860b263581625
cliftondavies <clifton.davies@outlook.com>
2021-03-06 21:44:06 +0000

    Implement about component

---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+const About = () => {
+  const paragraph = `Our mission is to build a worldwide sharing movement that reduces waste, saves precious resources and
+  eases the burden on landfills while enabling our members to benefit from the strength of a larger community.`;
+
+  return (
+    <div>
+      <header />
+
+      <main>
+        <p>{paragraph}</p>
+      </main>
+    </div>
+  );
+};
+
+export default About;
