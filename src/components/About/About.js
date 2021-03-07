f35b0d66260d05c2dc95bb7e9ebbfd9a9f95df60
cliftondavies <clifton.davies@outlook.com>
2021-03-07 05:02:12 +0000

    Style about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,11 @@
+import styles from './About.module.scss';
+
 const About = () => {
   const paragraph = `Our mission is to build a worldwide sharing movement that reduces waste, saves precious resources and
   eases the burden on landfills while enabling our members to benefit from the strength of a larger community.`;
 
   return (
-    <div>
+    <div className={styles.about}>
       <header />
 
       <main>
