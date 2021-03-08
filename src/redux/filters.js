6fcef828a86479bbcd5899a665eddb1c82c973e0
cliftondavies <clifton.davies@outlook.com>
2021-03-08 20:10:55 +0000

    Add fix for possible type error related to length property when no posts are returned

--- src/redux/filters.js
+++ src/redux/filters.js
@@ -1,4 +1,4 @@
 export default (state, filter) => {
   const { posts } = state.posts;
-  return (posts.length > 0) ? posts.filter(post => post.type === filter) : posts;
+  return (posts && posts.length > 0) ? posts.filter(post => post.type === filter) : posts;
 };
