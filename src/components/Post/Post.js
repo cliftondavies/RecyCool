287516971c71de57a816331c4bf1e0bddac7df10
cliftondavies <clifton.davies@outlook.com>
2021-03-09 00:29:35 +0000

    Refactor the general post component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,70 +1,6 @@
-import { Link } from 'react-router-dom';
-import faker from 'faker';
 import PropTypes from 'prop-types';
-import image from '../../assets/no-image.png';
-import styles from './Post.module.scss';
-
-const path = '/items';
-const dayText = amountOfDays => {
-  let dayText;
-
-  if (amountOfDays === 0) { dayText = 'today'; }
-  if (amountOfDays === 1) { dayText = `⌛ ${amountOfDays} day ago`; }
-  if (amountOfDays > 1) { dayText = `⌛ ${amountOfDays} days ago`; }
-
-  return dayText;
-};
-
-const OfferPost = ({ id, thumbnail, title }) => {
-  const days = Math.floor((Math.random() * 30));
-  const editedTitle = title.replace(/\(.*/gm, '');
-
-  return (
-    <article className={`${styles.post} ${styles.offer}`}>
-      <Link to={`${path}/${id}`} />
-
-      <img src={thumbnail || image} width="250" height="250" alt="thumbnail" />
-
-      <div>
-        <h3>{editedTitle}</h3>
-
-        <span>
-          {`📍 ${Math.floor((Math.random() * 10))} km`}
-        </span>
-        <span>
-          {`🌟 ${Math.floor((Math.random() * 5))}`}
-        </span>
-        <span>
-          {`👀 ${Math.floor((Math.random() * 2000))}`}
-        </span>
-        <span>
-          {dayText(days)}
-        </span>
-      </div>
-    </article>
-  );
-};
-
-const WantedPost = ({ id, content }) => {
-  const days = Math.floor((Math.random() * 30));
-  const editedContent = content.replace(/\s+http.+/g, '.');
-
-  return (
-    <article className={`${styles.post} ${styles.wanted}`}>
-      <Link to={`${path}/${id}`} />
-
-      <h3>
-        {`${faker.name.findName()} `}
-      </h3>
-
-      <p>{editedContent}</p>
-
-      <span>
-        {dayText(days)}
-      </span>
-    </article>
-  );
-};
+import OfferPost from '../OfferPost/OfferPost';
+import WantedPost from '../WantedPost/WantedPost';
 
 const Post = ({
   post, postType,
@@ -75,21 +11,6 @@ const Post = ({
   if (postType === 'offer') { return <OfferPost id={id} thumbnail={photo} title={title} />; }
 
   return <WantedPost id={id} content={content} />;
-};
-
-OfferPost.defaultProps = {
-  thumbnail: null,
-};
-
-OfferPost.propTypes = {
-  id: PropTypes.number.isRequired,
-  thumbnail: PropTypes.string,
-  title: PropTypes.string.isRequired,
-};
-
-WantedPost.propTypes = {
-  id: PropTypes.number.isRequired,
-  content: PropTypes.string.isRequired,
 };
 
 Post.propTypes = {
