268f365c4f434f847effb72e7656c6f02e6b07ce
cliftondavies <clifton.davies@outlook.com>
2021-05-10 19:53:49 +0100

    Implement mobile responsiveness

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,11 @@
-import { useLocation } from 'react-router-dom';
 import styles from './Footer.module.scss';
 
-const Footer = () => {
-  const location = useLocation();
-  let footerStyle = styles.footer;
-
-  if (location.state && location.state.wanted) { footerStyle = styles.wantedfooter; }
-
-  return (
-    <footer className={footerStyle}>
-      <span>
-        RecyCool by Clifton Davies © 2021
-      </span>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className={styles.footer}>
+    <span>
+      RecyCool by Clifton Davies © 2021
+    </span>
+  </footer>
+);
 
 export default Footer;
